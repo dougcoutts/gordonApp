40f23bec7e4e7544f5086802e7345d46e033e1ac
Douglas Coutts <doug.coutts@salishseadiving.com>
2017-10-26 13:18:04 -0700

    Changed home page to reflect version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CK Express' });
+  res.render('index', { title: 'Career Key Test Server' });
 });
 
 module.exports = router;
